2c52db44c70a84cdf4ee774c76c9db15285d15b2
D1ffic00lt <dm.filinov@gmail.com>
2024-10-24 13:30:21 +0300

    removed temporary environment variables

--- peers-generator/main.py
+++ peers-generator/main.py
@@ -4,16 +4,6 @@ import asyncio
 from utils import BotConfig, WG0
 from bot import VPNBot
 
-os.environ["TOKEN"] = "../secrets/telegram-token.txt"
-os.environ["PUID"]= "1000"
-os.environ["PGID"] = "1000"
-os.environ["TZ"] = "Europe/Moscow"
-os.environ["SERVERURL"] = "109.172.94.36"
-os.environ["SERVERPORT"] = "51820"
-os.environ["PEERS"] = "0"
-os.environ["PEERDNS"] = "true"
-os.environ["INTERNAL_SUBNET"] = "10.13.13.0"
-
 with open(os.environ.get('TOKEN')) as token_file:
     token = token_file.read().strip()
 
